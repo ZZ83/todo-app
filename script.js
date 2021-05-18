906c1a67d9f35c5338bbcba7658b7befe66b2941
Zekel Keys <zzkeys83@gmail.com>
2021-05-18 15:32:09 -0700

    Add themes object

---
+++ script.js
@@ -0,0 +1,16 @@
+const themes = {
+    dark: {
+        "--bg-color":             `#161722`,
+        "--items-bg-color":       `#25273c`,
+        "--primary-text-color":   `#cacde8`,
+        "--secondary-text-color": `#777a92`,
+        "--accent-color":         `#494C6B`,
+    },
+    light: {
+        "--bg-color":             `#FAFAFA`,
+        "--items-bg-color":       `#FFFFFF`,
+        "--primary-text-color":   `#494C6B`,
+        "--secondary-text-color": `#9495A5`,
+        "--accent-color":         `#E3E4F1`,
+    }
+}
